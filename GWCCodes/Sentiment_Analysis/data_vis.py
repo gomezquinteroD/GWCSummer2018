19b15981b8703d9309058dc654d31d872b62fc08
Daniela Gomez <gomezq.daniela@gmail.com>
2018-07-31 11:35:10 -0400

    complete part 1

--- GWCCodes/Sentiment_Analysis/data_vis.py
+++ GWCCodes/Sentiment_Analysis/data_vis.py
@@ -6,5 +6,20 @@ tweetFile = open("./tweets.json", "r")
 tweetData = json.load(tweetFile)
 tweetFile.close()
 
+#Continue program below
+#json object or tweets are stored in tweetData
+
+
+#iterate list of tweets
+    #for each tweet access text
+    #print polarity
+polarityList = []
+
+for tweet in tweetData:
+    tweetblob = TextBlob(tweet["text"])
+    polarityList.append(tweetblob.polarity)
+
+print(polarityList)
+
 tb = TextBlob("You are a brilliant computer scientist.")
 print(tb.polarity)
